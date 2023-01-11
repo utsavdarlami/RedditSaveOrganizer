cf5b081ebf35ce87edc9a94dc53a808a06eec61f
utsavdarlami <utsavdarlami17@gmail.com>
2023-01-11 23:35:19 +0545

    refactor: comments and global var remove

--- app/server.py
+++ app/server.py
@@ -1,30 +1,34 @@
-from flask import Flask, jsonify, render_template
+import json
 import os
 import re
-from db import mongo_connect
-from reddit import fetchAllSave
-import json
-from flask_cors import CORS
+
 from bson.json_util import ObjectId
+from db import mongo_connect
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+from reddit import fetchAllSave
 
 
 class MyEncoder(json.JSONEncoder):
     """
-        to make ObjectId serializable in the json retun
+    to make ObjectId serializable in the json retun
     """
+
     def default(self, obj):
         if isinstance(obj, ObjectId):
             return str(obj)
         return super(MyEncoder, self).default(obj)
+
 
 conn = mongo_connect()
 redditsave_db = conn["redditsave"]
 save_collection = redditsave_db["all_saves"]
 
 app = Flask(__name__)
-app.json_encoder = MyEncoder ## assingin the encoder
+app.json_encoder = MyEncoder  ## assingin the encoder
 
 CORS(app)
+
 
 @app.route("/")
 def index():
@@ -36,31 +40,40 @@ def runFetch():
     responseOfFetching = fetchAllSave()
     return jsonify(responseOfFetching)
 
-@app.route("/allsaves", methods=['GET'])
-def allSave():
-    # getting collection from mongodb
 
-    global save_collection
+@app.route("/allsaves", methods=["GET"])
+def allSave():
+    """Show all saves."""
+    # getting collection from mongodb
     allsaves_cursor = save_collection.find()
     allsaves_list = list(allsaves_cursor)
     return jsonify(allsaves_list)
 
+
 @app.route("/search/<subreddit>")
 def search(subreddit):
+    """
+    Search through the saves.
 
-    global save_collection
+    Currently supports search through
+    - [x] subreddit
 
-    posts_cursor = save_collection.find({ "subreddit": { "$regex": subreddit, "$options" :'i' } })
+    TODOs
+    -------
+    TODO: Search through title
+    """
+    posts_cursor = save_collection.find(
+        {"subreddit": {"$regex": subreddit, "$options": "i"}}
+    )
 
     posts_list = list(posts_cursor)
 
-    if len(posts_list)==0:
+    if len(posts_list) == 0:
         emptySearch = {"id": 404, "msg": "Error No Such Sub."}
         return jsonify(emptySearch)
     else:
         return jsonify(posts_list)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',
-            port=5000,
-            debug=os.environ.get('DEBUG') == '1')
+    app.run(host="0.0.0.0", port=5000, debug=os.environ.get("DEBUG") == "1")
