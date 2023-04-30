e3ca9c21c4c35b34ad01bbacfb2e426d13514173
Bhabuk <bhabukunwar@gmail.com>
2023-04-30 20:44:13 +0545

    mongodb-configured backed-proxy-fixed

--- app/db.py
+++ app/db.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 
 def mongo_connect():
     port = "27017"
-    MONGO_USERNAME = "admin"
-    MONGO_PASSWORD = "admin"
+    MONGO_USERNAME = "admin123"
+    MONGO_PASSWORD = "password123"
     MONGO_HOSTNAME = "mongo"
     mongo_link = f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOSTNAME}:{port}/'
     # mongo_link = f'mongodb://{host_name}:{port}/'
