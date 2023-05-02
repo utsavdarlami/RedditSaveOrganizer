d61a477bbe91b09621b51e1dacd69c79f6219325
Bhabuk <bhabukunwar@gmail.com>
2023-05-02 16:05:03 +0545

    mongodb connection fixed

--- app/db.py
+++ app/db.py
@@ -1,11 +1,12 @@
+# from loguru import logger
 from pymongo import MongoClient
 
-def mongo_connect():
+
+def mongo_connect(mongo_hostname="mongo"):
     port = "27017"
     MONGO_USERNAME = "admin123"
     MONGO_PASSWORD = "password123"
-    MONGO_HOSTNAME = "mongo"
-    mongo_link = f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOSTNAME}:{port}/'
-    # mongo_link = f'mongodb://{host_name}:{port}/'
-    # print(mongo_link)
+    MONGO_HOSTNAME = "mongodb"
+    mongo_link = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOSTNAME}:{port}/"
+    # logger.info(mongo_link)
     return MongoClient(mongo_link)
